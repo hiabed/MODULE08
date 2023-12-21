f7ac5bfac59892905426f3bfb64c4f2e153edf7e
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-21 14:44:27 +0100

    ex01 in progress

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,6 +1,18 @@
 # ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <iostream>
+# include <algorithm>
+# include <vector>
 
+template<typename T>
+
+T &easyfind(T &a, int n)
+{
+    T holder = find(a.begin(), a.end(), n);
+    if (holder != a.end())
+        return (holder);
+    throw std::runtime_error("Element not found");
+}
 
 # endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
-    
+    std::vector <int> arr = {5, 2, 4, 1};
+    try
+    {
+        easyfind(arr, 0);
+    }
+    catch (std::exception &e)
+    {
+        e.what();
+    }
 }
