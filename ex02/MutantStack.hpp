a6a5675af60a8f1e7efc8b7dbcc4fb31177bce9c
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-23 21:52:25 +0100

    ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,19 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <list>
+#include <vector>
+#include <iostream>
+
+template <typename T>
+
+class MutantStack : public std::stack<T>
+{
+private:
+    
+public:
+    
+};
+
+#endif
