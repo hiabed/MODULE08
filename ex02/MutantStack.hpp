cc34c29105619800ea5129b3aac2eea7c08bd5e5
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-24 15:28:26 +0100

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,16 +4,29 @@
 #include <stack>
 #include <list>
 #include <vector>
+#include <deque>
 #include <iostream>
 
 template <typename T>
 
 class MutantStack : public std::stack<T>
 {
-private:
-    
 public:
-    
+    typedef typename std::deque<T>::iterator iterator;
+
+    iterator begin()
+    {
+        return std::stack<T>::c.begin();
+    }
+    iterator end()
+    {
+        return std::stack<T>::c.end();
+    }
+    // orthodox canolical form;
+    MutantStack(){}
+    MutantStack(const MutantStack &other) { *this = other; }
+    MutantStack &operator=(const MutantStack &other) { (void)other; return *this; }
+    ~MutantStack() {}
 };
 
 #endif
